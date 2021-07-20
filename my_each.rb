6113abd4ab6290e686bceef566bd509a76300a9a
Ambar Khare <ambar.khare24@gmail.com>
2021-07-19 22:27:03 -0700

    commit tree b3143365eedf6c42cc422ffba59b7e4965c8e2fa

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+word = [1, 2, 3, 4]
+
+def my_each(word) 
+  if block_given?
+    i = 0
+    while i < word.size
+      yield(word[i])
+      i += 1
+    end
+    word
+  else
+    "There is no block"
+  end
 end
+
+my_each(word) { |words|
+words
+}
